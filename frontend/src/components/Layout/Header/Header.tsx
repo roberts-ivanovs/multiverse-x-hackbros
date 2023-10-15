287855bee5f286223717127e0502b17f66d44a39
Ronalds Palacis <ronalds.palacis@outlook.com>
2023-10-15 21:59:22 +0300

    feat: stash

---
+++ frontend/src/components/Layout/Header/Header.tsx
@@ -0,0 +1,51 @@
+import { Button } from 'components/Button';
+import { MxLink } from 'components/MxLink';
+import { environment } from 'config';
+import { logout } from 'helpers';
+import { useGetIsLoggedIn } from 'hooks';
+import { RouteNamesEnum } from 'localConstants';
+import MultiversXLogo from '../../../assets/img/multiversx-logo.svg?react';
+
+export const Header = () => {
+  const isLoggedIn = useGetIsLoggedIn();
+
+  const handleLogout = () => {
+    sessionStorage.clear();
+    logout(`${window.location.origin}/unlock`, undefined, false);
+  };
+
+  return (
+    <header className='flex flex-row align-center justify-between pl-6 pr-6 pt-6'>
+      <MxLink
+        className='flex items-center gap-5'
+        to={isLoggedIn ? RouteNamesEnum.dashboard : RouteNamesEnum.home}
+      >
+        <MultiversXLogo className='w-full h-6' />
+        <div className='h-full w-[1px] bg-gray-800' />
+        <h1 className='bg-gradient-to-r from-yellow-400 text-lg font-bold via-orange-500 to-yellow-400 text-transparent bg-clip-text animate-text'>
+          xChains
+        </h1>
+      </MxLink>
+
+      <nav className='h-full w-full text-sm sm:relative sm:left-auto sm:top-auto sm:flex sm:w-auto sm:flex-row sm:justify-end sm:bg-transparent'>
+        <div className='flex justify-end container mx-auto items-center gap-2'>
+          <div className='flex gap-1 items-center'>
+            <div className='w-2 h-2 rounded-full bg-green-500' />
+            <p className='text-white'>{environment}</p>
+          </div>
+
+          {isLoggedIn ? (
+            <Button
+              onClick={handleLogout}
+              className='inline-block rounded-lg px-3 py-2 text-center hover:no-underline my-0 text-gray-600 hover:bg-slate-100 mx-0'
+            >
+              Close
+            </Button>
+          ) : (
+            <MxLink to={RouteNamesEnum.unlock}>Connect</MxLink>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      animation: {
+        text: 'text 5s ease infinite'
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          }
+        }
+      },
+      fontFamily: {
+        sans: ['"Inter"', 'sans-serif']
+      },
+      colors: {
+        gray: {
+          800: '#292828',
+          900: '#171717'
+        }
+      }
+    }
+  },
+  plugins: []
+};
